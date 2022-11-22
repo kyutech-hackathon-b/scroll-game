24abb26c80d7b73f4d47570e78ccf655865b539f
MIYABETaisei <mytspe46@gmail.com>
2022-11-22 16:43:12 +0900

    remove: 不要な関数削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,29 +19,6 @@ export default function Home() {
   const [devicePixelRatio, setDevicePixelRatio] = useState(0);
   const [open, setOpen] = useState<boolean>(true);
 
-  const handleChangePixelRatio = useCallback(
-    function () {
-      // A function which will update the device pixel ratio of the Unity
-      // Application to match the device pixel ratio of the browser.
-      const updateDevicePixelRatio = function () {
-        setDevicePixelRatio(window.devicePixelRatio);
-      };
-      // A media matcher which watches for changes in the device pixel ratio.
-      const mediaMatcher = window.matchMedia(
-        `screen and (resolution: ${devicePixelRatio}dppx)`
-      );
-      // Adding an event listener to the media matcher which will update the
-      // device pixel ratio of the Unity Application when the device pixel
-      // ratio changes.
-      mediaMatcher.addEventListener("change", updateDevicePixelRatio);
-      return function () {
-        // Removing the event listener when the component unmounts.
-        mediaMatcher.removeEventListener("change", updateDevicePixelRatio);
-      };
-    },
-    [devicePixelRatio]
-  );
-
   useEffect(() => {
     if (typeof window !== "undefined") {
       setDevicePixelRatio(window.devicePixelRatio);
