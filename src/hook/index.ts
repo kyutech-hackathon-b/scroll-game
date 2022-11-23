693f63e837030499eea5f61d6b8c98417fc38d3f
MIYABETaisei <mytspe46@gmail.com>
2022-11-23 12:13:59 +0900

    update: コントラクトアドレスの変更

--- src/hook/index.ts
+++ src/hook/index.ts
@@ -1,8 +1,8 @@
 import { Contract, utils } from "ethers";
-import RewardNFTAbi from "../../contract/RewardNFT.json";
 import { RewardNFT } from "../../contract";
+import RewardNFTAbi from "../../contract/RewardNFT.json";
 
-const contractAddress = "0x1B815CFc267A6B7f91F2f8EdEC98C00547fa71df";
+const contractAddress = "0x54B6C7e79a80a24423ddDb32E821Fa2E88EcEca4";
 const RewardNFTInterface = new utils.Interface(RewardNFTAbi.abi);
 export const contract = new Contract(
   contractAddress,
